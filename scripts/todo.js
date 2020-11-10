d604314210f35d4d80166e372cba5f111006a251
Thu-Tammy <bigbabychicken@gmail.com>
2020-11-09 21:42:31 -0700

    add item list

--- scripts/todo.js
+++ scripts/todo.js
@@ -2,3 +2,20 @@ function sendAlert(message) {
 alert(message);
 }
 sendAlert("Hello World!");
+function addTodoItem() {
+    var todo = document.getElementById("todo-item");
+
+    //this.sendAlert(todo.value);
+
+    this.addToList(todo.value);
+
+    todo.value = null;
+}
+
+function addToList(message) {
+    var list = document.getElementById("todo-list");
+    var item = document.createElement("li");
+
+    item.appendChild(document.createTextNode(message));
+    list.appendChild(item);
+}
