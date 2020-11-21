2cfd2e6a24495f64e6abb14a335c1f0ff2f95e41
Thu-Tammy <bigbabychicken@gmail.com>
2020-11-21 07:39:48 -0700

    renew item

--- scripts/todo.js
+++ scripts/todo.js
@@ -50,7 +50,20 @@ function completeItem(item) {
 
   item.target.innerHTML = null;
   item.target.appendChild(strikeout);
+  item.target.removeEventListener("click", completeItem);
+  item.target.addEventListener("click", renewItem);
 }
+
+function renewItem(item) {
+  var message = item.target.innerHTML;
+  var listItem = item.target.parentNode;
+
+  listItem.innerHTML = null;
+  listItem.appendChild(document.createTextNode(message));
+  listItem.removeEventListener("click", renewItem);
+  listItem.addEventListener("click", completeItem);
+}
+
 function isExistingItem(message) {
   return this.todoItems.includes(message);
 }
